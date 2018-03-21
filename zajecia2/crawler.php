3111db4f065149ed1bcad126dd29530811177261
Karina Chojnacka <277945@zarz.agh.edu.pl>
2018-03-21 13:00:55 +0100

    zajecia 21.03

--- zajecia2/crawler.php
+++ zajecia2/crawler.php
@@ -14,13 +14,21 @@ $url = $_GET["search"];
 
 if (filter_var($url, FILTER_VALIDATE_URL)) {
     echo("$url is a valid URL");
+    $dom = new DOMDocument;
+
+$dom->loadHTMLFile($url);
 } else {
     echo("$url is not a valid URL");
 }
+
+$links = $url->getElementsByTagName('a');
+foreach ($links as $link) {
     
+$final_link= explode('#',$link->getAttribute( 'href' ));
     
-    nazwa_url=document.getElementsByTagName("a");
-    echo $nazwa_url;
+    //echo $link->nodeValue, PHP_EOL;
+}    
+
 ?>
 
 </body>
