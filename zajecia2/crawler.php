c673c115945f4927848f102cf965e2cd9f3500cb
Karina <karinaxx95@gmail.com>
2018-03-23 00:59:30 +0100

    dodano crawler.php

---
+++ zajecia2/crawler.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form action="" method="GET">
+    <input type="text" name="search">
+    <input type="submit" value="Crawl!">
+</form>
+    
+    
+
+    
+    <?php
+$url = $_GET["search"];
+if (filter_var($url, FILTER_VALIDATE_URL)) {
+    echo("$url is a valid URL");
+    $dom = new DOMDocument;
+$dom->loadHTMLFile($url);
+} else {
+    echo("$url is not a valid URL");
+}
+$links = $url->getElementsByTagName('a');
+foreach ($links as $link) {
+    
+$final_link= explode('#',$link->getAttribute( 'href' ));
+    
+    //echo $link->nodeValue, PHP_EOL;
+}    
+?>
+
+</body>
+</html
