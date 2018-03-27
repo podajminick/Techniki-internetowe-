e95486cb66de466b9e1f32cc98e583d56d307317
Karina <karinaxx95@gmail.com>
2018-03-27 18:03:53 +0200

    wersja ostateczna

--- zajecia2/crawler.php
+++ zajecia2/crawler.php
@@ -1,30 +1,47 @@
 <!DOCTYPE html>
 <html>
+<head>
+	<meta charset="UTF-8">
+	<title>Crawler</title>
+	<link rel="stylesheet" href="crawler.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
 <body>
+
+
 <form action="" method="GET">
+   <h1>Crawler</h1>
     <input type="text" name="search">
-    <input type="submit" value="Crawl!">
+    <input type="submit"  id="submit" value="Crawl!">
 </form>
     
-    
+     
 
     
-    <?php
-$url = $_GET["search"];
-if (filter_var($url, FILTER_VALIDATE_URL)) {
-    echo("$url is a valid URL");
-    $dom = new DOMDocument;
-$dom->loadHTMLFile($url);
-} else {
-    echo("$url is not a valid URL");
-}
-$links = $url->getElementsByTagName('a');
-foreach ($links as $link) {
-    
-$final_link= explode('#',$link->getAttribute( 'href' ));
-    
-    //echo $link->nodeValue, PHP_EOL;
-}    
+    <?php	
+	$url = (isset($_GET["search"]) ? $_GET["search"] : "");
+	//echo $url;
+	
+		if (filter_var($url, FILTER_VALIDATE_URL)) {
+    	//echo("$url is a valid URL");	
+			$html = ($url);
+			$dom = new DOMDocument();
+			@$dom->loadHTMLFile(($html));
+			$links = $dom->getElementsByTagName('a');
+			foreach ($links as $link){
+				
+				echo "<div>" . $link-> getAttribute('href')."</div>";
+				
+			}
+			
+		}
+	
+			else {
+			echo ("<p id='fault'> $url is not a valid URL  </p>"  );
+		}
+
+
+	
 ?>
 
 </body>
